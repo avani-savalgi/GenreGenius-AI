5669961af2d9ec6d677fc13b05b5e6933acbb5c7
Avani Savalgi <avanisavalagi@gmail.com>
2026-01-13 16:44:41 +0530

    fix: axois communication between my Vercel frontend and Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
     if (!data) return;
     setIsDownloading(true);
     try {
-      const response = await axios.post(`${API_BASE_URL}/api/generate-pdf`, data, { responseType: 'blob' });
+      const response = await axios.get(`${API_BASE_URL}/api/generate-pdf`, data, { responseType: 'blob' });
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
